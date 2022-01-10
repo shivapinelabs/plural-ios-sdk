b4c738e759bc640e60c77c7f761cf55468077812
ashwinivishwaspine <87471129+ashwinivishwaspine@users.noreply.github.com>
2022-01-10 16:33:59 +0530

    xcframework added

---
+++ PluralCheckout.xcframework/ios-arm64/PluralCheckout.framework/Headers/PluralCheckout.h
@@ -0,0 +1,18 @@
+//
+//  PluralCheckout.h
+//  PluralCheckout
+//
+//  Created by Ashwini Vishwas on 08/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PluralCheckout.
+FOUNDATION_EXPORT double PluralCheckoutVersionNumber;
+
+//! Project version string for PluralCheckout.
+FOUNDATION_EXPORT const unsigned char PluralCheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PluralCheckout/PublicHeader.h>
+
+
